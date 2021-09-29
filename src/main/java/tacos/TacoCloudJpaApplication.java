042939fcc56e8258b8e7a77b8f69c03f0148fdee
Marina Nesterenko <miss.nesterenko.marina@gmail.com>
2021-09-29 19:32:52 +0400

    begin ch03-jpa

---
+++ src/main/java/tacos/TacoCloudJpaApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudJpaApplication.class, args);
+    }
+
+}
